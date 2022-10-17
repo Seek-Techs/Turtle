47c514ccbc7a25b81b77c0f9de62f487da892def
Sikiru Yusuff <sikiru.yusuff@yahoo.com>
2022-10-17 15:12:57 +0100

    first commit

---
+++ turtle_graphic.py
@@ -0,0 +1,22 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor('lightgreen')
+
+# create player
+player = turtle.Turtle()
+player.color('blue')
+player.shape('triangle')
+player.penup()
+
+# set speed variable
+speed = 1
+
+while True: 
+    player.forward(speed)
+
+
+
+
+
+turtle.done()
